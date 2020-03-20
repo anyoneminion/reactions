e8834b6048c33f4c70097a089dec9336a4d12a2f
anyoneminion <62104085+anyoneminion@users.noreply.github.com>
2020-03-20 12:10:19 -0400

    upload dotblacklist for e8s

---
+++ dotblacklist.lua
@@ -0,0 +1,13 @@
+-- Persistent Data
+local multiRefObjects = {
+
+} -- multiRefObjects
+local obj1 = {
+	[8342] = true;
+	[8346] = true;
+	[8658] = true;
+	[9214] = true;
+	[9321] = true;
+	[9319] = true;
+}
+return obj1
